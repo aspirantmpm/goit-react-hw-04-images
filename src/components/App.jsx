5ea964f54c845c478f3a31d13c880696f479620b
Vadym <aspirantmpm@gmail.com>
2023-03-17 17:42:47 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Toaster, toast } from 'react-hot-toast';
 import fetchImages from './fetch';
 import { GlobalStyle } from './Globalstyle';
@@ -8,80 +9,71 @@ import { ImageGallery } from './ImageGallery';
 import { Button } from './Button';
 import { Modal } from './Modal';
 
-export class App extends Component {
-  state = {
-    searchInput: '',
-    page: 1,
-    isLoading: false,
-    images: null,
-    error: null,
-    totalHits: 0,
-    imagesOnPage: 0,
-    showModal: false,
-    currentLargeImageUrl: '',
-    currentImageTags: '',
-  };
+export const App = () => {
+  const[searchInput, setSearchInput] = useState('');
+  const[page, setPage] = useState(1);
+  const[isLoading, setIsLoading] = useState(false);
+  const [images, setImages] = useState(null);
+  // const [error, setError] = useState(null);
+  const [totalHits, setTotalHits] = useState(0);
+  const [imagesOnPage, setImagesOnPage] = useState(0);
+  const [showModal, setShowModal] = useState('');
+  const [currentLargeImageUrl, setCurrentLargeImageUrl] = useState('');
+  const [currentImageTags, setCurrentImageTags] = useState('');
+  // state = {
+  //   searchInput: '',
+  //   page: 1,
+  //   isLoading: false,
+  //   images: null,
+  //   error: null,
+  //   totalHits: 0,
+  //   imagesOnPage: 0,
+  //   showModal: false,
+  //   currentLargeImageUrl: '',
+  //   currentImageTags: '',
+  // };
 
-  componentDidUpdate(_, prevState) {
-    const prevQuery = prevState.searchInput;
-    const nextQuery = this.state.searchInput;
-    const prevPage = prevState.page;
-    const nextPage = this.state.page;
+  useEffect(() => {
+    if (searchInput !== '') {
+      setIsLoading(true);
 
-    if (nextQuery !== prevQuery) {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-      this.setState({ isLoading: true });
-
-      fetchImages(nextQuery, nextPage)
+      fetchImages(searchInput, page)
         .then(({ hits, totalHits }) => {
           if (hits.length === 0) {
-            this.setState({ images: null, imagesOnPage: 0, totalHits: 0 });
-            return toast.error(`There is no image with name ${nextQuery}`);
+            setImages(null);
+            setTotalHits(0);
+            return Promise.reject(
+              toast.error(`There is no image with name ${searchInput}`)
+            );
           }
-          const arrayOfImages = this.createArrayOfImages(hits);
 
-          this.setState({
-            images: arrayOfImages,
-            totalHits,
-            imagesOnPage: hits.length,
-          });
+          const arrayOfImages = createArrayOfImages(hits);
+
+          setTotalHits(totalHits);
+          setImagesOnPage(hits.length);
+
+          return arrayOfImages;
         })
-        
+        .then(arrayOfImages => {
+          if (page === 1) {
+            setImages(arrayOfImages);
+            window.scrollTo({
+              top: 0,
+            });
+            return;
+          }
+          setImages(prevImages => [...prevImages, ...arrayOfImages]);
+        })
+
         .catch(error => {
-          this.setState({ error });
-          toast.error('Sorry, something went wrong. Please try again later.');
+          console.log(`${error.message}`);
         })
-        .finally(() => {
-          this.turnOffLoader();
-        });
+
+        .finally(() => turnOffLoader());
     }
+  }, [page, searchInput]);
 
-    if (nextPage > prevPage) {
-      this.setState({ isLoading: true });
-
-      fetchImages(nextQuery, nextPage)
-        .then(({ hits }) => {
-          const arrayOfImages = this.createArrayOfImages(hits);
-
-          this.setState(prevState => {
-            return { images: [...prevState.images, ...arrayOfImages] };
-          });
-          this.setState({
-            imagesOnPage: this.state.images.length,
-          });
-        })
-        .catch(error => {
-          this.setState({ error });
-          toast.error('Sorry, something went wrong. Please try again later.');
-        })
-        .finally(() => this.turnOffLoader());
-    }
-  }
-
-  createArrayOfImages = data => {
+  const createArrayOfImages = data => {
     const arrayOfImages = data.map(element => ({
       tags: element.tags,
       webformatURL: element.webformatURL,
@@ -90,43 +82,44 @@ export class App extends Component {
     return arrayOfImages;
   };
 
-  turnOffLoader = () => {
-    return this.setState({ isLoading: false });
+  const turnOffLoader = () => {
+    return setIsLoading( false );
   };
 
-  formSubmitHandler = data => {
-    this.setState({ searchInput: data, page: 1 });
+  const formSubmitHandler = data => {
+    setSearchInput(data);
+    setPage(1)
   };
 
-  nextFetch = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
+  const nextFetch = () => {
+    setPage(prevState => prevState + 1 );
+    ;
   };
 
-  openModal = event => {
+  const openModal = event => {
     const currentLargeImageUrl = event.target.dataset.large;
     const currentImageTags = event.target.alt;
+    setCurrentLargeImageUrl(currentLargeImageUrl);
+    setCurrentImageTags(currentImageTags);
 
-    this.setState({ currentLargeImageUrl, currentImageTags });
-    this.toggleModal();
+    // this.setState({ currentLargeImageUrl, currentImageTags });
+    toggleModal();
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+  const toggleModal = () => {
+    setShowModal( showModal  =>  !showModal,
+    );
   };
 
-  render() {
-    const {
-      images,
-      isLoading,
-      showModal,
-      currentLargeImageUrl,
-      currentImageTags,
-      imagesOnPage,
-    } = this.state;
+  // render() {
+    // const {
+    //   images,
+    //   isLoading,
+    //   showModal,
+    //   currentLargeImageUrl,
+    //   currentImageTags,
+    //   imagesOnPage,
+    // } = this.state;
 
     return (
       <div>
@@ -137,24 +130,24 @@ export class App extends Component {
             duration: 2000,
           }}
         />
-        <Searchbar onSubmit={this.formSubmitHandler} />
-        {images && <ImageGallery images={images} openModal={this.openModal} />}
+        <Searchbar onSubmit={formSubmitHandler} />
+        {images && <ImageGallery images={images} openModal={openModal} />}
 
         {isLoading && <Loader />}
 
         {imagesOnPage > 0 &&
-          this.state.page < Math.ceil(this.state.totalHits / 12) && (
-            <Button onClick={this.nextFetch} />
+          page < Math.ceil(totalHits / 12) && (
+            <Button onClick={nextFetch} />
           )}
 
         {showModal && (
           <Modal
             imageUrl={currentLargeImageUrl}
             imageTags={currentImageTags}
-            onClose={this.toggleModal}
+            onClose={toggleModal}
           />
         )}
       </div>
     );
-  }
+  // }
 }
